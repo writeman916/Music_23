23d65a186be1358d1fdd95f86da6c3885b5b7732
writeman <ngvietnam.6266@gmai.com>
2018-08-02 10:36:24 +0700

    design homepage

--- app/src/main/java/com/framgia/music_23/screen/home/HomeFragment.java
+++ app/src/main/java/com/framgia/music_23/screen/home/HomeFragment.java
@@ -16,7 +16,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
